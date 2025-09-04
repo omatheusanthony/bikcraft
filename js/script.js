fcb9239eeb3e531282d8e400bfbcf7fddd54a2a3
Matheus Anthony <omatheusanthony@gmail.com>
2025-09-04 19:51:30 -0300

    Acabou

--- js/script.js
+++ js/script.js
@@ -48,4 +48,21 @@ perguntas.forEach(eventosPerguntas);
 const galeria = document.querySelectorAll(".bicicleta-imagens img");
 const galeriaContainer = document.querySelector(".bicicleta-imagens");
 
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 920px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
 
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
